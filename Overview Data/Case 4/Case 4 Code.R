d1f93f8d18f4f011cba24feb55b6b949f29c0f9e
J-D-J-Clare <jclare2@wisc.edu>
2020-06-09 13:34:57 -0500

    Add files via upload

--- Overview Data/Case 4/Case 4 Code.R
+++ Overview Data/Case 4/Case 4 Code.R
@@ -2,7 +2,7 @@
 Behav_Class<-read.csv('Behav_Class.csv')
 
 
-
+library(mgcv)
 ###model for foraging
 m1<-bam(cbind(forage, npics)~TRAIL+te(X, Y, Day, bs=c('cr', 'cr', 'cc'), k=c(5, 5, 20))+s(Camera, bs='re')+
           s(DailyEVI, k=20, bs='cr')+s(DailyResEVI, k=20, bs='cr')+s(IntEVI, k=20, bs='cr')+
@@ -27,3 +27,16 @@ m2<-bam(cbind(vigilant, npics)~TRAIL+te(X, Y, Day, bs=c('cr', 'cr', 'cc'), k=c(5
           s(RichnessL35km, k=20, bs='cr')+
           s(Cropland, k=20, bs='cr'), 
         data=Behav_Class, family=binomial(), discrete=TRUE)
+
+
+###here, description of the creation of DailyEVI, DailyResEVI, IntEVI variables.
+###Recall, double-logistic function used to estimate phenology parameters based upon calculated Veg Indices:
+###Minimum VI, Maximum VI, SOS (start of rise), EOS (start of descent), RISE (slope of rise), FALL (slope of descent).
+###*DailyEVI* is a predicted (smoothed) estimate of the EVI for a given pixel on a given day using the parameter estimates
+###And the double logistic estimating equation (Beck et al. 2006).
+###*IntEVI* is the sum of all DailyEVI values for a specific pixel b/w the estimated SOS and EOS.
+###*DailyResEVI* is the difference between DailyEVI at a given pixel, and the concurrent mean Daily EVI of the the 8 surrounding pixels.
+
+###Daily Snow Depth derived from Snodas layers--resampled to 500 m grid to align with existing pixels.
+
+
