5bd0c2c5dbf6edb2391a54f06376b94e21885fc1
J-D-J-Clare <jclare2@wisc.edu>
2020-06-09 13:12:13 -0500

    Add files via upload

--- Overview Data/Case 3/Case 3 Code.R
+++ Overview Data/Case 3/Case 3 Code.R
@@ -116,11 +116,78 @@ gbm_COYOTE_MS<-gbm.step(data=Cam_Loc_Summary_Env,
                         tree.complexity=5, learning.rate=0.01, bag.fraction=.5)
 
 ###Briefly, creation of spatial layers.
-###Original grid size off 500 x 500 m MODIS pixels used for EVI, NDVI vars.
-###Land Cover proportions use a polygon extraction from base layers (Wiscland2.0).
-###Same extraction used to generate edge density and Shannon, and richness metrics.
-###Richness calculated based on level 3 land classes. 
-###Larger aggregations (e.g., 1km, 5km, 10km suffixes above) derived using raster::focal and raster::focalWeight....
+###Original grid size off 500 x 500 m MODIS pixels used directly for EVI, NDVI, snow layers
+###Nighttime lights cells originally 1 km resolution, resampled to 500m
+###Land Cover proportions use a polygon (the 500m Modis grid) extraction from base layers (Wiscland2.0).
+###Same extraction used to generate edge density, Aggregation,  Shannon, and richness metrics.
+###Richness and Shannon's Diversity Index calculated based on level 3 land classes. 
+###Edge Density, Core Area, Aggregation Index calculated based on level 1 land classes
+###Larger proportional aggregations (e.g., 1km, 5km, 10km suffixes above) derived using raster::focal and raster::focalWeight....
 ###...e.g., fw1km<-focalweight(r, 1000, 'circle); Pine1km<-(Pine, fw1km, mean)
+###Landscape metrics at different scales derived by reclipping the landscape (either original 500 m cell, or circular buffers
+###surrounding centroid of original cell. Edge Density, Core Area, Aggregation Index are 'landscape stats', i.e., derived 
+###(summed, averaged, etc.) across all patches and cover classes
+###Richness and Shannon diversity acriss larger extents reflect averages (eg., mean richness within the 500 m pixels in 
+###a 5 km buffer surrounding the centroid of the focal cell)
 
+###Variable explanations:
+###Response Variables
+#COYOTEDetsPA-Coyote detected (1) or not (0)
+#OPOSSUMDetsPA-Opossum detected (1) or not (0)
+#SKUNKSTRIPEDDetsPA-Striped Skunk detected (1) or not (0)
+###Phenology Metrics
+###Max, Min, SOS (day growing season starts), LOS (day growing season ends) are parameters derived from double logistic model
+###fit to vegetation indices (Enhanced Vegetation Index, Normalized Difference Vegetation Index) calculated from 16-d r. data
+###Values here denote averaged estimates from years 2014 through 2017.
+#MaxEVI
+#MinEVI
+#SOSEVI
+#EOSEVI
+#LOSEVI
+#MaxNDVI
+#MinNDVI
+#SOSNDVI
+#EOSNDVI
+#LOSNDVI
+#IntEVI
+###Climate/Snow
+#JanLST--This is the average land surface--coverted to f--termperature across January from 2001-2017
+#JanMaxSnow--Averaged 8day Maximum Snow Extent sampled across January from 2001-2017.
+###Nightlight--Raw nighttime-lights reflectance
+###Landscape Configuration Metrics (described above; Wiscland 2.0)
+#EdgeDensity
+#AgIndex
+#CoreArea
+###Landscape Composition (Wiscland 2.0)
+## % Class --What percent each class (note some redudancy...forest include deciduous, which includes aspend_birch...)
+#Aspen_Birch
+#Broadleaf
+#Coniferous
+#ConiferousWetland
+#Cropland
+#Developed
+#Forest
+#ForestedWetland
+#Grassland
+#HardwoodWetland
+#HerbWetland
+#Oak
+#Pine
+#propJPine
+#propRPine
+#propWater
+#propSMaple
+#propCHW
+#propNHW
+#Wetland
+#propSpruceFir
+##Composition summaries (WiscLand 2.0)
+#ShannonL3
+#RichnessL3 
+###Other
+#trapnights-how long each point sampled
+#X scaled X coordinate
+#Y scaled Y coordinate
 
+###Definition of a 'site' here is the specific camera location. However, note point-specific attributes calculated using a point-based extraction.
+###In other words, 2 cameras 150 m apart that fall within the same Modis pixel will have the same attributes.
